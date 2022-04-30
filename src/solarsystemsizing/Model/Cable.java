d24c27c658bc928fe70dafaf12387bdddbfd5e78
allenbagai <allenkamadje@gmail.com>
2022-05-01 00:35:16 +0200

    creating class Cable.java variables with their getter methods

--- src/solarsystemsizing/Model/Cable.java
+++ src/solarsystemsizing/Model/Cable.java
@@ -1,5 +1,21 @@
 package solarsystemsizing.Model;
 
 public class Cable {
-    
+    private String cableClass;
+    private float resistivity;
+
+    /**
+     * @return String return the cableClass
+     */
+    public String getCableClass() {
+        return cableClass;
+    }
+
+    /**
+     * @return float return the resistivity
+     */
+    public float getResistivity() {
+        return resistivity;
+    }
+
 }
